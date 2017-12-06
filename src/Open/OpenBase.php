5316a87fb67e9fed1495b0ebba840c94789ae7d5
zhanjh <zhanjh@126.com>
2017-12-06 20:04:00 +0800

    remove jsonResponse function in rest & open

--- src/Open/OpenBase.php
+++ src/Open/OpenBase.php
@@ -1,25 +1,7 @@
 <?php
 namespace Gap\Base\Open;
 
-use Gap\Http\JsonResponse;
-
 abstract class OpenBase
 {
     use \Gap\Base\Controller\MainTrait;
-
-    // deprecated
-    protected function jsonResponse($data = null, int $status = 200, array $headers = array())
-    {
-        return new JsonResponse($data, $status, $headers);
-    }
-
-    protected function getOAuth2Server()
-    {
-        return $this->app->get('oauth2Server');
-    }
-
-    protected function getOAuth2Storage()
-    {
-        return $this->app->get('oauth2Storage');
-    }
 }
--- src/Rest/RestBase.php
+++ src/Rest/RestBase.php
@@ -1,15 +1,7 @@
 <?php
 namespace Gap\Base\Rest;
 
-use Gap\Http\JsonResponse;
-
 abstract class RestBase
 {
     use \Gap\Base\Controller\MainTrait;
-
-    // deprecated
-    protected function jsonResponse($data = null, int $status = 200, array $headers = array())
-    {
-        return new JsonResponse($data, $status, $headers);
-    }
 }
