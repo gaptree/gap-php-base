0e48373e7a3dd17b85e5fd5d8fe27c0e5bb84f04
Jason <changyuanjian@gmail.com>
2018-01-09 19:09:22 +0800

    mod: add RouteFilter/PrivilegeFilter
    Test Plan: no test
    
    Reviewers: zjh
    
    Differential Revision: https://project.tecposter.cn/D1188

---
+++ src/RouteFilter/PrivilegeFilter.php
@@ -0,0 +1,39 @@
+<?php
+namespace Gap\Base\RouteFilter;
+
+use Gap\Base\Exception\NotLoginException;
+use Gap\Base\Exception\NoPermissionException;
+
+class PrivilegeFilter extends RouteFilterBase
+{
+    public function filter()
+    {
+        $config = $this->app->getConfig();
+        $access = $this->route->getAccess();
+        $privilege = $config->get("secure.privilege.$access");
+
+        if (!$privilege) {
+            return;
+        }
+
+        $serviceClass = $config->get('secure.fetchUserService');
+        if (!class_exists($serviceClass)) {
+            throw new \Exception('secure.fetchUserService not-found: ' . $serviceClass);
+        }
+
+        $fetchUserService = new $serviceClass($this->app);
+
+        $userId = $this->request->getUserId();
+        if (!$userId) {
+            throw new NotLoginException("access-$access-need-login");
+        }
+        $user = $fetchUserService->fetchOneByUserId($userId);
+        if (!$user) {
+            throw new NotLoginException("userId[$userId]-not-found");
+        }
+
+        if ($user->getPrivilege() < $privilege) {
+            throw new NoPermissionException("access-$access");
+        }
+    }
+}
