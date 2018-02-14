9d7e575b4ff372b9f7e044c5ec30d3f029c125a3
zhanjh <zhanjh@126.com>
2018-02-15 01:28:42 +0800

    App::get, set -> getDatabaseManager, getCacheManager ...

--- src/App.php
+++ src/App.php
@@ -2,23 +2,90 @@
 namespace Gap\Base;
 
 use Gap\Config\Config;
+use Gap\Database\DatabaseManager;
+use Gap\Cache\CacheManager;
+
+use Gap\I18n\Locale\LocaleManager;
+use Gap\I18n\Translator\Translator;
 
 class App
 {
     protected $config;
-    protected $singletons = [];
-    protected $refs = []; // reflection class & functions
+    protected $databaseManager;
+    protected $cacheManager;
+
+    protected $localeManager;
+    protected $translator;
+
+    //protected $singletons = [];
+    //protected $refs = []; // reflection class & functions
 
     public function __construct(Config $config)
     {
         $this->config = $config;
     }
 
-    public function getConfig()
+    public function getConfig(): Config
     {
         return $this->config;
     }
 
+    public function getDatabaseManager(): DatabaseManager
+    {
+        if ($this->databaseManager) {
+            return $this->databaseManager;
+        }
+
+        $this->databaseManager = new DatabaseManager(
+            $this->config->get('db'),
+            $this->config->get('server.id')
+        );
+        return $this->databaseManager;
+    }
+
+    public function getCacheManager(): CacheManager
+    {
+        if ($this->cacheManager) {
+            return $this->cacheManager;
+        }
+
+        $this->cacheManager = new CacheManager(
+            $this->config->get('cache')
+        );
+        return $this->cacheManager;
+    }
+
+    public function getLocaleManager(): LocaleManager
+    {
+        if ($this->localeManager) {
+            return $this->localeManager;
+        }
+
+        $localeOpts = $this->config->get('i18n.locale');
+        /*
+        if (empty($localeOpts)) {
+            throw new \Exception('cannot find config i18n.locale');
+        }
+        */
+        $this->localeManager = new LocaleManager($localeOpts);
+        return $this->localeManager;
+    }
+
+    public function getTranslator(): Translator
+    {
+        if ($this->translator) {
+            return $this->translator;
+        }
+
+        $this->translator = new Translator(
+            $this->getDatabaseManager()->connect($this->config->get('i18n.db')),
+            $this->getCacheManager()->connect($this->config->get('i18n.cache'))
+        );
+        return $this->translator;
+    }
+
+
+    /*
     public function set($key, $ref, $args = [])
     {
         $this->refs[$key] = [$ref, $args];
@@ -61,4 +128,5 @@ class App
 
         throw new \Exception("Error reflection format for $key in App");
     }
+    */
 }
