08b823eea0688e083debe88ee9874f758f1992cd
zhanjh <zhanjh@126.com>
2018-05-11 16:10:47 +0800

    OpenBase::json & OpenBase::errorJson

--- src/Open/OpenBase.php
+++ src/Open/OpenBase.php
@@ -2,7 +2,19 @@
 namespace Gap\Base\Open;
 
 use Gap\Base\Controller\ControllerBase;
+use Gap\Http\JsonResponse;
 
 abstract class OpenBase extends ControllerBase
 {
+    public function json(\JsonSerializable $obj): JsonResponse
+    {
+        return new JsonResponse($obj);
+    }
+
+    public function errorJson(\JsonSerializable $obj): JsonResponse
+    {
+        $response = new JsonResponse($obj);
+        $response->setStatusCode(JsonResponse::HTTP_BAD_REQUEST);
+        return $response;
+    }
 }
